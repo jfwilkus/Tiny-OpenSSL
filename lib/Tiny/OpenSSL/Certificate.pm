f1f2511a2ccd6281edff5d72d7f02d16e3b06e4c
James F Wilkus <jfwilkus@mac.com>
2014-10-06 11:07:48 -0500

    move self_sign to Certificate

--- lib/Tiny/OpenSSL/Certificate.pm
+++ lib/Tiny/OpenSSL/Certificate.pm
@@ -7,7 +7,10 @@ package Tiny::OpenSSL::Certificate;
 # VERSION
 
 use Moo;
+use Carp;
 use Types::Standard qw( InstanceOf );
+use Tiny::OpenSSL::Config qw($CONFIG);
+use Capture::Tiny qw( :all );
 
 with 'Tiny::OpenSSL::Role::Entity';
 
@@ -15,6 +18,41 @@ has [qw(issuer subject)] =>
     ( is => 'rw', isa => InstanceOf ['Tiny::OpenSSL::Subject'] );
 
 has key => ( is => 'rw', isa => InstanceOf ['Tiny::OpenSSL::Key'] );
+
+sub self_sign {
+
+    my $self = shift;
+    my $csr  = shift;
+
+    if ( !defined $csr ) {
+        croak 'csr is not defined';
+    }
+
+    my $pass_file = Path::Tiny->tempfile;
+    $pass_file->spew( $self->key->password );
+
+    my @args = (
+        'x509',     '-req',
+        '-days',    $CONFIG->{ca}{days},
+        '-in',      $csr->file,
+        '-signkey', $self->key->file,
+        '-passin', sprintf( 'file:%s', $pass_file ),
+        '-out',    $self->file
+    );
+
+    my ( $stdout, $stderr, $exit ) = capture {
+        system( $CONFIG->{openssl}, @args );
+    };
+
+    if ( $exit != 0 ) {
+        croak( sprintf( 'cannot sign certificate: %s', $stderr ) );
+    }
+
+    $self->issuer( $self->subject );
+    $self->ascii( $self->file->slurp );
+
+    return 1;
+}
 
 1;
 
@@ -29,3 +67,9 @@ A Tiny::OpenSSL::Subject object for the subject of the certificate.
 =method key
 
 A Tiny::OpenSSL::Key object.
+
+=method self_sign
+
+Self sign certificate.
+
+    $cert->self_sign($csr);
--- lib/Tiny/OpenSSL/CertificateAuthority.pm
+++ lib/Tiny/OpenSSL/CertificateAuthority.pm
@@ -7,7 +7,6 @@ package Tiny::OpenSSL::CertificateAuthority;
 # VERSION
 
 use Moo;
-use Carp;
 use Capture::Tiny qw( :all );
 use Tiny::OpenSSL::Config qw($CONFIG);
 
@@ -15,41 +14,6 @@ extends 'Tiny::OpenSSL::Certificate';
 
 sub sign {
     my $self = shift;
-
-    return 1;
-}
-
-sub self_sign {
-
-    my $self = shift;
-    my $csr  = shift;
-
-    if ( !defined $csr ) {
-        croak 'csr is not defined';
-    }
-
-    my $pass_file = Path::Tiny->tempfile;
-    $pass_file->spew( $self->key->password );
-
-    my @args = (
-        'x509',     '-req',
-        '-days',    $CONFIG->{ca}{days},
-        '-in',      $csr->file,
-        '-signkey', $self->key->file,
-        '-passin', sprintf( 'file:%s', $pass_file ),
-        '-out',    $self->file
-    );
-
-    my ( $stdout, $stderr, $exit ) = capture {
-        system( $CONFIG->{openssl}, @args );
-    };
-
-    if ( $exit != 0 ) {
-        croak( sprintf( 'cannot sign certificate: %s', $stderr ) );
-    }
-
-    $self->issuer( $self->subject );
-    $self->ascii( $self->file->slurp );
 
     return 1;
 }
@@ -63,8 +27,3 @@ Sign a certificate.
 
     my $ca->sign($csr);
 
-=method self_sign
-
-Self sign certificate.
-
-    my $ca->self_sign($csr);
